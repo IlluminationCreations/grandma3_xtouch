00cd0540a9478fc3c693c1d7fb17e2181d9f6414
Alexia W <alex@illumination-creations.com>
2024-05-28 18:43:28 -0700

    Limit max page count to 99

--- src/include/XController.h
+++ src/include/XController.h
@@ -6,7 +6,8 @@
 
 constexpr unsigned short xt_port = 10111;
 constexpr unsigned int PHYSICAL_CHANNEL_COUNT = 8;
-constexpr unsigned int MAX_PAGE_COUNT = 9999;
+// constexpr unsigned int MAX_PAGE_COUNT = 9999;
+constexpr unsigned int MAX_PAGE_COUNT = 99; // TODO: Limiting to 99 as the assignment display only has 2 digits. Do we really need 9999 pages?
 constexpr unsigned int MAX_CHANNEL_COUNT = 90;
 enum class ControlType { UNKNOWN, SEGMENT, FADER, KNOB };
 enum class SegmentID { UNKNOWN, PAGE };
